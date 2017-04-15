697d0f6afd8dfe81dfad988efda9214be4149cfe
eydandash <esraadandash96@me.com>
2017-04-15 21:53:05 +0200

    last milestone in the game

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,60 +1,113 @@
-/*
- This is the game class where the game is implemented. It is a memory game that simply generates a random pattern of planets everytime the user 
-presses the space bar, then the user has to shoot the same sequence of planetsq (right to left) with the rocket, which is moved using the up, right 
-and left non-keypad arrows. For now the sun actually sits right in the middle of the 8 planets, but it is to be used later on such 
-that if the rocket hits it, the rocket lives decrement.
-There is a really crucial bit when running the game, the space bar has to be pressed at least once before any other button, otherwise the 
-planetlist will not be initialised and when the space (the gameWorld) tries to compare different planets it is going to throw a NullPointerException. 
-For every collision the user will see either a small tick or a cross as a result, the console will also print out the result.
- */
+
+
+
+ 
 package game;
 
-import city.cs.engine.DebugViewer;
+import city.cs.engine.SoundClip;
 import city.cs.engine.UserView;
-import java.awt.Color;
+
+import java.io.IOException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 
 /**
- *
- * @author Esraadandash
- */
-public class Game {
-private final Space space;
-public UserView view;
+ * @author      Esraa, Dandash, esraa.dandash@city.ac.uk
+ * @version     1.0
+ * @since       1.0
+*/
 
+public class Game  {
 
-public Game() {
+    protected GameLevel space;
+    public UserView view;
+    public int levelno;
+    public Collided c;
+    protected Controller controller;
+    private GameOverWindow go;
+    private SoundClip gameOver;
+    protected JFrame frame;
+    private PauseResumeWindow pr;
 
-        // make the world
-        space= new Space();
+    /**
+     * This method starts the first Level of the game.
+     */
+    public Game() {
 
-        view = new Background(space, space.getPlayer(), 1000, 700);
         
-                //view.setGridResolution(1);
-
-        final JFrame frame = new JFrame("Memory Rocket");
+        space=new Level3(this);
+        levelno=1;
+        space.playBackgroundMusic();
+        c = new Collided(space.getPlayer(), space);
+        c.setGame(this);
+        
+        frame = new JFrame("Memory Rocket");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationByPlatform(true);
         frame.add(view);
+       
         frame.setResizable(false);
         frame.pack();
         frame.setVisible(true);
-        
-        JFrame debugView = new DebugViewer(space, 1000, 700);
-        space.start();        
+        //JFrame debugView = new DebugViewer(space, 1000, 700);
+        space.start();
 
-        
-        frame.addKeyListener(new Controller(space.getPlayer(),space));
+        controller = new Controller(space.getPlayer(), space);
+        frame.addKeyListener(controller);
 
-   
-        
-        
     }
 
-public static void main(String[] args){
+    
 
-new Game();        
+    /**
+     * This method is invoked when the the user hits the P button on the keyboard.
+     */
+    public void pauseResume(){
+        
+        pr= new PauseResumeWindow();
+         pr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         pr.pack();
+         pr.setVisible(true);
+        }
 
-}
+    /**
+     * This method will stop the current version only in this version, in next versions it is intended to move the game directly to the next level.
+     */
+    public void goNextLevel(){
+        space.stop();
+        }
+
+    /**
+     * This method is invoked when the rocket's lives equal 0.
+     */
+    public void gameOver(){
+             space.gameMusic.stop();
+             try {
+            gameOver = new SoundClip("data/gameover.mp3");
+            gameOver.play();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.out.println("Sound file not found!");
+        }
+             space.stop();
+             frame.setVisible(false);
+         go= new GameOverWindow();
+         go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         go.pack();
+         go.setVisible(true);
+         }
+
+    /**
+     * This is the main method for the game
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        new Game();
+
+    }
+
+    
+
 }
